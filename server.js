f72128f1b4aab8ee49657ed87b865b5f797b57d7
Thorsten Maaß <Thor@Thorstens-MBP.fritz.box>
2018-01-28 23:20:08 +0100

    änderung

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 var express = require('express');
 var app = express();
 app.use(function(req, res, next) {
+	console.log('Server gestartet!');
 	console.log(req.url);
 	next();
 });
-app.use('/', express.static(__dirname));
+app.use('/', express.static(__dirname + '/public'));
 app.get('/speziell', function(req, res) {
 	console.log('Eine Spezielle Datei wird ausgeliefert!');
 	res.sendfile(__dirname + '/speziell.html');
 });
 app.get('/*', function(req, res) {
-	res.status(404).sendfile(__dirname + '/error.html');
+	res.status(404).sendfile(__dirname + '/public/error.html');
 });
-app.listen(64163); 
+app.listen(3000); 
